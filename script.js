e8bbc99addc2b05ca3406805d7eb68d39e5c6708
asta4pasta <asta4pasta@gmail.com>
2019-05-14 21:54:34 +0300

    pridetas swiperis reviews skiltyje

---
+++ script.js
@@ -0,0 +1,11 @@
+  //Swiper
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  })
