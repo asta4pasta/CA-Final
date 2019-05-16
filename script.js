35fac6034c82646590490e69b7f8fcd9ac9a92f4
asta4pasta <asta4pasta@gmail.com>
2019-05-17 00:05:50 +0300

    sutvarkytas slideris reviews skiltyje

--- script.js
+++ script.js
@@ -1,11 +1,69 @@
-  //Swiper
-  var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
+// /////////////////////////////////////////
+// //Smooth-scroll'as
+// document.querySelectorAll('a[href^="#"]').forEach($anchor => {
+//   $anchor.addEventListener('click', function (e){
+//     e.preventDefault();
+//     document.querySelector(this.getAttribute('href')).scrollIntoView({
+//       behaviour:'smooth',
+//       block: 'start'
+//     })
+//   })
+// })
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  })
+
+// ////////////////////////////////////
+// //Swiper Features skiltyje
+
+// let mySSwiper = new Swiper ('.swiper-container-interactive', {
+//   direction: 'horizontal',
+//   loop: false,
+//   slidesPerView: 1,
+//   clickable: true,
+// })
+
+// let tabs = document.querySelectorAll(".tab");
+// //ciklas kuriame zymimi mygtuku ir slide'u active'ai
+//   for (let i = 0; i < tabs.length; i++) {
+
+//     // kas vyksta paspaudus
+//     tabs[i].addEventListener("click", function() {
+
+//       // Nuimami visi active'ai
+//       for (let j = 0; j < tabs.length; j++){
+//         tabs[j].classList.remove("active");
+//       }
+
+//       // uzdedamas active and pirmo mygtuko
+//       this.classList.add("active");
+
+//       // keiciami slaidai naudojantis atributais "data-slide" kurie yra html'e ant mygtuku
+//       let slideNumber = this.getAttribute("data-slide");
+//       mySSwiper.slideTo(slideNumber-1, 300);
+//     })
+//   }
+
+//   //cia kai keiciasi slide'as keiciasi ir mygtukas, vice versa
+//   mySSwiper.on("slideChange", function () {
+//     //susiejamas mygtuko ir slide'o indexai
+//     let slideIndex = mySSwiper.activeIndex;
+//     //nuimami active'ai nuo mygtuku kurie nera paspausti
+//     for (let j = 0; j < tabs.length; j++){
+//      tabs[j].classList.remove("active");
+//     }
+//     //uzdedamas active'as ant mygtuko kuris paspaustas
+//     tabs[slideIndex].classList.add("active");
+//    });
+
+
+   
+
+ //////////////////////////////////////////
+//Swiper reviews skiltyje
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
